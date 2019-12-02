902e03027bb2b358e2aea7d88dfb02da2bcc281e
fabrizioronzino <fabryronzi@gmail.com>
2019-12-02 12:42:58 +0100

    updates

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -17,7 +17,7 @@ class Caltech(VisionDataset):
     def __init__(self, root, split="train", transform=None, target_transform=None):
         super(Caltech, self).__init__(root, transform=transform, target_transform=target_transform)
         
-        if ( split == "train" or split == "test"):
+        if ( self.split == "train" or self.split == "test"):
             
             self.split = self.split + ".txt"
             print(self.split)
