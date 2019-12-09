18a0495d6eb79372197b58c780d3f79b2ca9717e
fabrironzi <fabryronzi@gmail.com>
2019-12-09 10:08:03 +0100

    Update caltech_dataset.py
    update

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -18,7 +18,6 @@ class Caltech(VisionDataset):
         super(Caltech, self).__init__(root, transform=transform, target_transform=target_transform)
         
         print("Ciaooo")
-        stop
 # =============================================================================
 #         print(self.split)
 #         
