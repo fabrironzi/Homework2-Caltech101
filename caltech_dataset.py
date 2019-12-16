c3512c09db59a4a133194c7b60d05fa907a2c5b7
fabrironzi <fabryronzi@gmail.com>
2019-12-16 12:56:00 +0100

    Update caltech_dataset.py

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -1,13 +1,11 @@
 from torchvision.datasets import VisionDataset
-import re
-import os
+
 from PIL import Image
+
 import os
 import os.path
 import sys
 import torch
-import copy
-
 
 def pil_loader(path):
     # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
@@ -17,35 +15,64 @@ def pil_loader(path):
 
 
 class Caltech(VisionDataset):
-    def __init__(self, root, transform=None, target_transform=None):  #
-        super(Caltech, self).__init__(root,
-                                      transform=transform,
-                                      target_transform=target_transform)
+    
+    set_categories = set()
+    images = []
+    transform = None
 
-        self.list_categories = []
-        self.images = []
+    
+    def __init__(self, root, split="train", transform=None, target_transform=None):
+        super(Caltech, self).__init__(root, transform=transform, target_transform=target_transform)
+        
         self.transform = transform
 
-        list_dir = os.listdir(root)
-        list_dir.pop(list_dir.index('BACKGROUND_Google'))
+        if ( split == "train" or split == "test"):
+            
+            self.split = "Homework2_Caltech101/" + str(split) + ".txt"
+            #print(self.split)
+            
+            #with open("Homework2_Caltech101/train.txt", 'r') as fp:
+            with open(self.split, 'r') as fp:
+                content = fp.readline()
+                
+                for content in fp:
+                    
+                    if(content.startswith("BACKGROUND_Google")):
+                        print('Repository')
+                        continue
+                    
+                    data = ["./Homework2_Caltech101/101_ObjectCategories/" + content.strip()]
+                    self.set_categories.add(content.split("/")[0])
+                    data.append(list(self.set_categories).index(content.split("/")[0]))
+                    
+                    try:
+                        image_loaded = pil_loader(data[0])
+                        data[0] = image_loaded
+                        data[0] = self.transform(data[0])
+                    except Exception as e:
+                        print(e)
+                        
+                    self.images.append(data)
+                    
+            print(data)
+                
+               
+            fp.close()         
+            
 
-        for cat in list_dir:
-            self.list_categories.append(cat.lower().strip())
-
-        self.list_categories = sorted(self.list_categories)
-
-        for cat in list_dir:
-            for image in os.listdir("%s/%s" % (root,cat)):
-                image_location = ("%s/%s/%s" % (root,cat,image))
-                category_index = self.list_categories.index(cat.lower().strip())
-                self.images.append([image_location,category_index])
-
-    def get_category_list(self):
-        cat_copied = copy.deepcopy(self.list_categories)
-        return cat_copied
-
-    def get_cagetory_image(self, index):
-        return self.list_categories[self.images[index][1]]
+# =============================================================================
+#         self.split = split # This defines the split you are going to use
+#                            # (split files are called 'train.txt' and 'test.txt')
+# 
+# =============================================================================
+        '''
+        - Here you should implement the logic for reading the splits files and accessing elements
+        - If the RAM size allows it, it is faster to store all data in memory
+        - PyTorch Dataset classes use indexes to read elements
+        - You should provide a way for the __getitem__ method to access the image-label pair
+          through the index
+        - Labels should start from 0, so for Caltech you will have lables 0...100 (excluding the background class) 
+        '''
 
     def __getitem__(self, index):
         '''
@@ -57,19 +84,24 @@ class Caltech(VisionDataset):
             tuple: (sample, target) where target is class_index of the target class.
         '''
 
-        #image, label = ...  # Provide a way to access image and label via index
-        # Image should be a PIL Image
-        # label can be int
+        image, label = self.images[index]
+        
+        if not isinstance(image, torch.Tensor):
+            image = pil_loader(image)
+            image = self.transform(image)
+            
+        
+        
+        
+        # Provide a way to access image and label via index
+                           # Image should be a PIL Image
+                           # label can be int
 
         # Applies preprocessing when accessing the image
-        #if self.transform is not None:
-        #    image = self.transform(image)
-
-        image, label = self.images[index]
-
-        image = pil_loader(image)
-        if self.transform is not None:
-            image = self.transform(image)
+# =============================================================================
+#         if self.transform is not None:
+#             image = self.transform(image)
+# =============================================================================
 
         return image, label
 
@@ -79,4 +111,6 @@ class Caltech(VisionDataset):
         It is mandatory, as this is used by several other components
         '''
         length = len(self.images)
+        
+        # Provide a way to get the length (number of elements) of the dataset
         return length
